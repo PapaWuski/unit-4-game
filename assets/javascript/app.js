3157e1ed9cba4c338424d027cf4d75c967ce22e4
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-09-02 22:10:22 -0700

    change from images to cards

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,88 @@
-var $pTag = $("<p>")
-var $root = $(".container")
-var $imgTag = $("<img>")
+var $pTag = $("<p>");
+var $root = $(".container");
+var $btn = $("<button>");
+var $row = $("<div>");
+var $col = $("<div>");
+var char = {
+  0: {
+    name: "asdf",
+    hp: 20,
+    atk: 5,
+    def: 5
+  },
+  1: {
+    name: "asdf2",
+    hp: 20,
+    atk: 5,
+    def: 5
+  },
+  2: {
+    name: "asdf3",
+    hp: 20,
+    atk: 5,
+    def: 5
+  },
+  3: {
+    name: "asd3",
+    hp: 20,
+    atk: 5,
+    def: 5
+  }
+};
 
-var char0 = {
-    hp:20,
-    atk:5,
-    def:5
-}
-var char1 = {
-    hp:20,
-    atk:5,
-    def:5
-}
-var char2 = {
-    hp:20,
-    atk:5,
-    def:5
-}
-var char3 = {
-    hp:20,
-    atk:5,
-    def:5
+function createChar(num, target) {
+  for (var i = 0; i < num; i++) {
+    $col = $("<div>")
+    $($col).attr("class", `col-md-3 characters char${i}`);
+    var $card = $("<div>");
+    $($card).addClass(`card`);
+    var $cardBody = $("<div>").addClass("card-body")
+    var $cardName = $("<h5>");
+    $($cardName).addClass("card-title");
+    $($cardName).text(`${char[i].name}`);
+    var $imgTag = $("<img>");
+    $($imgTag).attr("class", 'card-img-top');
+    $($imgTag).attr("src", `https://picsum.photos/id/${i}/200`);
+    $($cardBody).append($imgTag);
+    $($cardBody).append($cardName);
+    $($card).append($cardBody);
+    $($col).append($card);
+    $(target).append($col);
+
+  }
 }
 
-
-function createImages(num,target) {
-    for (var i=0; i< num; i++ ){
-        $imgTag = $("<img>")
-        $($imgTag).attr("class", `img-thumbnail characters char${i}`);
-        $($imgTag).attr("src", `https://picsum.photos/id/${i}/200`);
-        $(target).append($imgTag);
+function generateBoard() {
+  for (var i = 0; i < 3; i++) {
+    $row = $("<div>");
+    if (i === 0) {
+        createChar(4, $row);
     }
-}
-createImages(4)
-
-function generateBoard(){
-    for (let i = 0; i < 3; i++) {
-        var $row = $("<div>")
-        var $col = $("<div>")
-        $($col).attr("class", `col-md-12`);
-        if (i===0){
-        createImages(4,$col)
-        }
-        $($row).attr("class", `row row${i}`);
-        $($row).append($col);
-        $($root).append($row);
-    }
-
+    $($row).attr("class", `row row${i}`);
+    $($root).append($row);
+  }
+  $($btn).addClass("attack");
+  $(".row2").append($btn);
+  $($btn).text("Attack!");
+  $($btn).hide();
 }
 
-function moveChars() {
-    
-}
+generateBoard();
 
-generateBoard()
-
-$(document).on("click",".characters", function () {
-    $("img").removeClass("characters");
-    $(this).addClass("mainCharacter");
-    $("img").not(this).addClass("enemy");
-    $(".row1").append($(".enemy"));
+$(document).on("click", ".characters", function() {
+    $(".characters")
+      .not(this)
+      .addClass("enemy");
+  $(".characters").removeClass("characters");
+  $(this).addClass("mainCharacter");
+  $(".row1").append($(".enemy"));
 });
 
-
-$(document).on("click",".enemy", function () {
-    $(".row2").append(this);
-    $(this).addClass("currentTarget");
-    $("img").not(this).removeClass("enemy");
+$(document).on("click", ".enemy", function() {
+    $(".enemy")
+      .not(this)
+      .removeClass("enemy");
+  $(".row2").append(this);
+  $(this).addClass("currentTarget");
+  $($btn).show();
 });
