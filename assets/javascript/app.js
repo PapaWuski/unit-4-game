8aad39ef3805d20b350668384f99771dabc0c84d
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-09-04 12:31:27 -0700

    added onclick for attack class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ var $col = $("<div>");
 var char = {
   0: {
     name: "asdf",
-    hp: 20,
+    hp: 50,
     atk: 5,
     def: 5
   },
@@ -36,15 +36,25 @@ function createChar(num, target) {
     var $cardName = $("<h3>");
     var $card = $("<div>");
     var $imgTag = $("<img>");
+    var $cardText = $("<p>")
     $col = $("<div>")
     $($card).addClass(`card`);
     $($col).attr("class", `col-md-3 characters char${i}`);
+
     $($cardName).addClass("card-title");
     $($cardName).text(`${char[i].name}`);
+
     $($imgTag).attr("class", 'card-img-top');
     $($imgTag).attr("src", `https://picsum.photos/id/${i}/200`);
+
+    $($cardText).addClass("card-text");
+    $($cardText).text(`${char[i].hp}`);
+    $($cardText).attr("atk",`${char[i].atk}`);
+    $($cardText).attr("def",`${char[i].def}`);
+
     $($cardBody).append($imgTag);
     $($cardBody).append($cardName);
+    $($cardBody).append($cardText);
     $($card).append($cardBody);
     $($col).append($card);
     $(target).append($col);
@@ -62,6 +72,7 @@ function generateBoard() {
     $($root).append($row);
   }
   $col = $("<div>")
+  $btn = $("<btn>")
   $($col).addClass("col-md-3");
   $($btn).addClass("attack btn-lg btn-primary btn m-1");
   $($btn).text("Attack!");
@@ -71,6 +82,8 @@ function generateBoard() {
 }
 
 generateBoard();
+
+
 
 $(document).on("click", ".characters", function() {
     $(".characters")
@@ -85,14 +98,45 @@ $(document).on("click", ".characters", function() {
 
 $(document).on("click", ".enemy", function() {
     $(".enemy")
-      .not(this)
-      .removeClass("enemy");
+      .not(this).addClass("standby").removeClass("enemy");
+      
+    $(".enemy")
+      .not(this);
+
   $(".row3").append(this);
   $(this).addClass("currentTarget");
     $(".currentTarget .card").removeClass("bg-danger");
     $(".currentTarget .card").addClass("bg-warning");
-  $($btn).show();
+  $(".attack").show();
 });
-$(document).on("click","attack", function () {
-    $(selector).val();
+
+$(document).on("click",".attack", function () {
+    let playerAttack = $(".mainCharacter .card-text").attr("atk");
+    let playerHealth = $(".mainCharacter .card-text").text();
+    const enemyAttack = $(".currentTarget .card-text").attr("def");
+    let enemyHealth = $(".currentTarget .card-text").text();
+    enemyHealth -= playerAttack;
+    $(".currentTarget .card-text").text(enemyHealth);
+    if (enemyHealth <= 0) {
+      $(".currentTarget").attr("class","dead");
+      $(".dead").hide();
+      $(".standby").addClass("enemy").removeClass("standby");
+      const remainingEnemies = $(".enemy").length + $(".standby").length;
+      if (!remainingEnemies){
+        console.log("youwin")
+      }
+      return null
+    }
+    playerHealth -= enemyAttack;
+    $(".mainCharacter .card-text").text(playerHealth);
+    if (playerHealth <= 0) {
+      $(".container").empty();
+      generateBoard()
+      return null
+    }
+
+});
+
+$(document).on("click","reset", function () {
+  
 });
